ebdd30a12f5fdbab3b6c7b4dd7bc6d66b8d10309
kilroyblockchain <karen@kilroyblockchain.com>
2023-12-27 12:35:53 -0600

    dev my file baby login test

--- src/authConfig.js
+++ src/authConfig.js
@@ -6,9 +6,14 @@ const msalConfig = {
 //        clientId: "2be92905-5f2c-49bb-a1ba-3664388dd3a8",
 //        authority: "https://login.microsoftonline.com/73bc0c96-da02-4596-acb7-953c57ed55ac",
 //        redirectUri: "https://my.file.baby"
-        clientId: "cecf9ab2-8682-4ad6-9f85-3812fe5efba0",
-        authority: "https://login.microsoftonline.com/73bc0c96-da02-4596-acb7-953c57ed55ac",
-       redirectUri: "https://dev-my.file.baby"
+//        clientId: "cecf9ab2-8682-4ad6-9f85-3812fe5efba0",
+//        authority: "https://login.microsoftonline.com/73bc0c96-da02-4596-acb7-953c57ed55ac",
+//       redirectUri: "https://dev-my.file.baby"
+//      test to log in using file baby tenant
+        clientId: "247f5d06-8776-4fa6-9cc4-390826e2704c",
+        authority: "https://login.microsoftonline.com/1cb7b2f8-2a75-4a9c-82a0-43d42a2e5b5e",
+        redirectUri: "https://dev-my.file.baby"
+
     },
     cache: {
         cacheLocation: "localStorage",
