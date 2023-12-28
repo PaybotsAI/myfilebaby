89c053df01e2ac4f7a8f027828b89a2cfba2d35e
kilroyblockchain <karen@kilroyblockchain.com>
2023-12-27 22:11:12 -0600

    auth url

--- src/authConfig.js
+++ src/authConfig.js
@@ -13,7 +13,7 @@ const msalConfig = {
         clientId: "247f5d06-8776-4fa6-9cc4-390826e2704c",
  //       authority: "https://login.microsoftonline.com/1cb7b2f8-2a75-4a9c-82a0-43d42a2e5b5e",
 
-        authority: "https://filebaby.b2clogin.com/filebaby.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1_SignUpOrSignIn",
+        authority: "https://filebaby.b2clogin.com/filebaby.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_susi",
 //        authority: "https://filebaby.b2clogin.com/filebaby.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_SignUpOrSignIn&client_id=247f5d06-8776-4fa6-9cc4-390826e2704c&nonce=defaultNonce&redirect_uri=https%3A%2F%2Fdev-my.file.baby&scope=openid&response_type=code&prompt=login&code_challenge_method=S256&code_challenge=lxktjEhT1an5CaAk1Ts7xX_DoV1IqX5paL2ZGSJ6Y3A",
         redirectUri: "https://dev-my.file.baby"
 
