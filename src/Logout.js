78a1a840ce2833abbe306d1c37dad886791ee1a1
kilroyblockchain <karen@kilroyblockchain.com>
2023-12-29 14:58:26 -0600

    logout

--- src/Logout.js
+++ src/Logout.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Logout = () => {
     // Define your Azure AD tenant ID and the post logout redirect URI
     const tenantId = 'cecf9ab2-8682-4ad6-9f85-3812fe5efba0';
-    const postLogoutRedirectUri = 'https://dev-my.file.baby'; // Adjust as needed
+    const postLogoutRedirectUri = 'https://file.baby'; // Adjust as needed
 
     // Define the function to handle the logout
     const handleLogout = () => {
